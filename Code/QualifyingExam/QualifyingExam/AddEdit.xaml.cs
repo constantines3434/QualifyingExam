7ea460bca796049c2883dbcbe582b66b5b6967bd
Constantine Smirnov <constantines748@gmail.com>
2023-11-06 02:43:34 +0300

    Начало Задание 1

---
+++ Code/QualifyingExam/QualifyingExam/AddEdit.xaml.cs
@@ -0,0 +1,44 @@
+﻿using QualifyingExam.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QualifyingExam
+{
+    public partial class AddEdit : Page
+    {
+        Student localStudent;
+        public AddEdit()
+        {
+            InitializeComponent();
+            //if (student != null)
+            //    localStudent = student;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            localStudent.Surname = SurnameTextBox.Text;
+            localStudent.Name = NameTextBox.Text;
+            localStudent.Patronimic = PatronimicTextBox.Text;
+            localStudent.MathGrade = (int)MathGradeComboBox.SelectedItem;
+            localStudent.Informatics = (int)InformaticsGradeComboBox.SelectedItem;
+            localStudent.Physics = (int)PhysicsGradeComboBox.SelectedItem;
+
+            if (NavigationService.CanGoBack)
+            {
+                NavigationService.GoBack();
+            }
+        }
+    }
+}
---
+++ Code/QualifyingExam/QualifyingExam/Model/Model.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualifyingExam.Model
+{
+    public class Student
+    {
+        int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronimic { get; set; }
+        public int MathGrade { get; set; }
+        public int Informatics { get; set; }
+        public int Physics { get; set; }
+    }
+}
